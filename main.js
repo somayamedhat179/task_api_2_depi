777b5c62c3ccc13237bd24ee357809716e1bcc26
Somaya Medhat <somayamedhat179@gmail.com>
2025-09-21 14:59:44 +0200

    first commit

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-fetch("https://forkify-api.herokuapp.com/api/search?q=pizza")
-.then( function(result){
-  return result.json() ;
-})
-.then( function(json){ // json here is a variable 
-    var recipes = json.recipes ; // array of object
+fetch("https://dummyjson.com/products")
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (json) {
+    var products = json.products;
+    var myText = "";
 
-    var myText = ""; // empty string
-
-      for (var i = 0; i < recipes.length; i++) {
-        var box = `
+    for (var i = 0; i < products.length; i++) {
+      var box = `
           <div class="col-md-4 text-center my-3" >
-            <img src="${recipes[i].image_url}"class="w-100" style="height:350px">
-            <h6>${recipes[i].title}</h6>
-            <h5>${recipes[i].publisher}</h5>
-            <h5>${recipes[i].id}</h5>
+            <img src="${products[i].images[0]}"class="w-100" style="height:350px">
+            <h6>${products[i].title}</h6>
+            <h5>${products[i].category}</h5>
+            <h5>${products[i].price}$</h5>
+            <h5>${products[i].rating}</h5>
+            <h5>${products[i].id}</h5>
           </div> 
         `;
-        myText += box; 
-      }
+      myText += box;
+    }
 
-      document.querySelector(".test").innerHTML = myText;
-
-    })
+    document.querySelector(".test").innerHTML = myText;
+  });
